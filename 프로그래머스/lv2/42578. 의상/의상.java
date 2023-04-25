233ca20a5d24a9c5214f191fae746a7013079d95
Mincheol Shin <48898994+its-sky@users.noreply.github.com>
2023-04-26 01:22:37 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        
+        for (int i=0; i<clothes.length; i++) {
+            String key = clothes[i][1];
+            if (map.containsKey(key)) {
+                int cnt = map.remove(key);
+                map.put(key, cnt+1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        
+        for (String key : map.keySet()) {
+            answer = answer * (map.get(key) + 1);
+        }
+        
+        answer -= 1;
+        return answer;
+    }
+}
