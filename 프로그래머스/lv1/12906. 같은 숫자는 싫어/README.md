9b285d11b8a13754d140b911a61038e3d26102c0
Mincheol Shin <48898994+its-sky@users.noreply.github.com>
2023-04-29 12:00:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.36 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 34.26 ms
+메모리: 117 MB, 시간: 35.36 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -4,13 +4,14 @@ public class Solution {
     public int[] solution(int []arr) {
         ArrayList<Integer> list = new ArrayList<>();
         int value = -1;
-        for (int i=0; i < arr.length; i++) {
+        
+        for (int i=0; i<arr.length; i++) {
             if (arr[i] != value) {
                 list.add(arr[i]);
                 value = arr[i];
             }
         }
-
+        
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
