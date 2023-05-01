98217de1fbff2a311186ac2b7c23b1ec798c082f
Mincheol Shin <48898994+its-sky@users.noreply.github.com>
2023-05-01 16:11:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.78 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+        
+        Arrays.sort(arr, new Comparator<String>() {
+            
+            public int compare(String o1, String o2) {
+                return Integer.compare(Integer.parseInt(o1), Integer.parseInt(o2));
+            }
+        });
+        
+        answer = arr[0] + " " + arr[arr.length - 1];
+        
+        return answer;
+    }
+}
