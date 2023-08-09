cd4238278eebf99ccc8ce254ebffa75ad552f49b
Mincheol Shin <48898994+its-sky@users.noreply.github.com>
2023-08-09 13:32:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.50 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    static int[] parent;
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        Arrays.sort(costs, (int[] c1, int[] c2) -> c1[2]-c2[2]);
+        
+        parent = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        for (int[] edge : costs) {
+            int from = edge[0];
+            int to = edge[1];
+            int weight = edge[2];
+            
+            int fromParent = findParent(from);
+            int toParent = findParent(to);
+            
+            if (fromParent == toParent) continue;
+            
+            answer += weight;
+            parent[toParent] = fromParent;
+        }
+        
+        return answer;
+    }
+    
+    private int findParent(int node) {
+        if (parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+}
+
+// 최소 신장 트리 - MST
